ef5a5c29e70a553a6c4515b67df360c055141dae
VilelaMarcus <marcusvilela000@gmail.com>
2024-01-30 23:43:43 -0300

    deploy 1 attempt

--- src/routes/os-routes.ts
+++ src/routes/os-routes.ts
@@ -7,10 +7,13 @@ import createOsHandler from '../feature/OS/create-os-handler';
 import deleteOsHandler from '../feature/OS/delete-os-by-id';
 import updateOsHandler from '../feature/OS/update-os-handler';
 
+import createNewVisitByOShandler from '../feature/mobile/visit-measurment-os-handles';
+
 const osROuter = Router();
 
 osROuter.get('/',  createRoute(getOsHandler));
 osROuter.post('/',  createRoute(createOsHandler));
+osROuter.post('/visit-measurment', createRoute(createNewVisitByOShandler));
 osROuter.get('/:id', createRoute(getOsByLaserId));
 osROuter.put('/:id', createRoute(updateOsHandler));
 osROuter.delete('/:id', createRoute(deleteOsHandler));
