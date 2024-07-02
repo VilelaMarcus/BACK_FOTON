9c6f0ac1f7377e8b5f976d4d96ff3df0e7cf1fba
VilelaMarcus <marcusvilela000@gmail.com>
2024-07-02 14:02:20 -0300

    comment tunnel

--- src/tunnel.js
+++ src/tunnel.js
@@ -1,33 +1,33 @@
-import {createTunnel} from 'tunnel-ssh';
+// import {createTunnel} from 'tunnel-ssh';
 
-const sshOptions = {
-    host: '100.27.232.104',
-	port: 22,
-    username: 'ec2-user',
-    privateKey: process.env.SSH_KEY,
-};
+// const sshOptions = {
+//     host: '100.27.232.104',
+// 	port: 22,
+//     username: 'ec2-user',
+//     privateKey: process.env.SSH_KEY,
+// };
 
-function rdsTunnel(sshOptions, port, autoClose = false){
-    let forwardOptions = {
-        srcAddr: '127.0.0.1',
-        srcPort: port,
-        dstAddr: 'foton.c3iotoqdtfhm.us-east-1.rds.amazonaws.com',
-        dstPort: port
-    }
+// function rdsTunnel(sshOptions, port, autoClose = false){
+//     let forwardOptions = {
+//         srcAddr: '127.0.0.1',
+//         srcPort: port,
+//         dstAddr: 'foton.c3iotoqdtfhm.us-east-1.rds.amazonaws.com',
+//         dstPort: port
+//     }
 
-    let tunnelOptions = {
-        autoClose:autoClose
-    }
+//     let tunnelOptions = {
+//         autoClose:autoClose
+//     }
     
-    let serverOptions = {
-        port: port
-    }
+//     let serverOptions = {
+//         port: port
+//     }
 
-    return createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions);
-}
+//     return createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions);
+// }
 
-const init = async () => {
-    return await rdsTunnel(sshOptions,5432);
-};
+// const init = async () => {
+//     return await rdsTunnel(sshOptions,5432);
+// };
 
-export default { init };
+// export default { init };
