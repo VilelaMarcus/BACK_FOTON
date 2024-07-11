002a7f0ec0a5b12747ea2ba05b21d73c6ce74293
VilelaMarcus <marcusvilela000@gmail.com>
2024-07-11 11:57:22 -0300

    small changes to deploy

--- ci/create-prisma-layer.sh
+++ ci/create-prisma-layer.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Clearing out old layer..."
 rm -rf .prisma-layer
 
 echo "Generating Prisma Client"
-sh ci/generate-prisma.sh
+sh $DIR/generate-prisma.sh
 
 echo "Creating Prisma Layer..."
 mkdir -p .prisma-layer/nodejs/node_modules/.prisma
@@ -17,6 +22,6 @@ cp -r prisma .prisma-layer/nodejs/prisma
 
 echo "Copying over the OpenSSL 1.0.x Binary to the generic client folder in the layer"
 mkdir -p .prisma-layer/nodejs/node_modules/.prisma/client
-cp -f ./libquery_engine-rhel-openssl-1.0.x.so.node .prisma-layer/nodejs/node_modules/.prisma/client
+cp -f $DIR/libquery_engine-rhel-openssl-1.0.x.so.node .prisma-layer/nodejs/node_modules/.prisma/client
 
 echo "Layer Built"
